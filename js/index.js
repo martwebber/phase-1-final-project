359dfb6944468cf04b9701f931eeab38278187be
Martin Mwangi <mwangi.martin24@gmail.com>
2022-06-21 15:52:10 +0300

    Add movie poster and info to the DOM

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 const getMovies = () =>{
 fetch('http://localhost:3000/movies/?i=tt3896198')
 	.then(response => response.json())
-	.then(response => console.log(response))
+	.then(response => renderPage(response))
 	.catch(err => console.error(err));
 
 }
 
-const renderPage = () => {
+const renderPage = (movie) => {
 	const searchByIdForm = `
 	
 	<div class="row">
@@ -41,6 +41,109 @@ const renderPage = () => {
 		</div>
 	</div>
 	`
+	const resultsCard = `
+	
+	<div id="poster">
+                    <img src="${movie[0].Poster}" alt="">
+                </div>
+                <div id="movie-info">
+                    <table>
+                        <tr>
+                            <td>Title:</td>
+                            <td>${movie[0].Title}</td>
+                        </tr>
+                        <tr>
+                            <td>Year:</td>
+                            <td>${movie[0].Year}</td>
+                        </tr>
+                        <tr>
+                            <td>Rated:</td>
+                            <td>${movie[0].Rated}</td>
+                        </tr>
+                        <tr>
+                            <td>Released:</td>
+                            <td>${movie[0].Released}</td>
+                        </tr>
+                        <tr>
+                            <td>Runtime:</td>
+                            <td>${movie[0].Runtime}</td>
+                        </tr>
+                        <tr>
+                            <td>Genre:</td>
+                            <td>${movie[0].Genre}</td>
+                        </tr>
+                        <tr>
+                            <td>Director:</td>
+                            <td>${movie[0].Director}</td>
+                        </tr>
+                        <tr>
+                            <td>Writer:</td>
+                            <td>${movie[0].Writer}</td>
+                        </tr>
+                        <tr>
+                            <td>Actors:</td>
+                            <td>${movie[0].Actors}</td>
+                        </tr>
+                        <tr>
+                            <td>Plot:</td>
+                            <td>${movie[0].Plot}</td>
+                        </tr>
+                        <tr>
+                            <td>Language:</td>
+                            <td>${movie[0].Language}</td>
+                        </tr>
+                        <tr>
+                            <td>Country:</td>
+                            <td>${movie[0].Country}</td>
+                        </tr>
+                        <tr>
+                            <td>Awards:</td>
+                            <td>${movie[0].Awards}</td>
+                        </tr>
+                        <tr>
+                            <td>Ratings:</td>
+                            <td>United States</td>
+                        </tr>
+                        <tr>
+                            <td>Metascore: </td>
+                            <td>${movie[0].Metascore}</td>
+                        </tr>
+                        <tr>
+                            <td>ImdbRating:</td>
+                            <td>${movie[0].imdbRating}</td>
+                        </tr>
+                        <tr>
+                            <td>Imdb Votes</td>
+                            <td>${movie[0].imdbVotes}</td>
+                        </tr>
+                        <tr>
+                            <td>ImdbID</td>
+                            <td>${movie[0].imdbID}</td>
+                        </tr>
+                        <tr>
+                            <td>Type</td>
+                            <td>${movie[0].Type}</td>
+                        </tr>
+                        <tr>
+                            <td>DVD</td>
+                            <td>${movie[0].DVD}</td>
+                        </tr>
+                        <tr>
+                            <td>BoxOffice</td>
+                            <td>${movie[0].BoxOffice}</td>
+                        </tr>
+                        <tr>
+                            <td>Production</td>
+                            <td>${movie[0].Production}</td>
+                        </tr>
+                        <tr>
+                            <td>Website</td>
+                            <td>${movie[0].Website}</td>
+                        </tr>
+                    </table>
+                </div>
+
+	`
 const container = document.querySelector('#movie-container')
 
 	let selectEl = document.getElementById('search-type')
@@ -55,12 +158,13 @@ const container = document.querySelector('#movie-container')
 			container.innerHTML = searchByTitleForm           
 			document.querySelector('#container').appendChild(container)		}
 	  });
-
+	  container.innerHTML = resultsCard
 }
 
 const init = () =>{
 	document.addEventListener('DOMContentLoaded',()=>{
-		renderPage()
+		//renderPage()
+		getMovies()
 	})
 }
 init()
